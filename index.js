f3055ba1ae7f5003746220a76e920891a4021548
Ken Bailey <kenneth.ross.bailey@gmail.com>
2016-01-02 15:59:19 -0800

    Added more to ignore and addedpackage.json

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-/**
- * Created by kenbailey on 1/2/16.
- */
+var express = require('express');
+var app = express();
+
+app.use(express.static('web'));
+
+app.get('/api', function (req, res) {
+    res.send('Hello Homify API!');
+});
+
+app.listen(3000, function () {
+    console.log('Homify is now listening on port 3000');
+});
