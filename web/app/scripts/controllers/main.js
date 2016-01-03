bdceb31d69c9146f92bc44a29f1f55e4e58029c2
gtg489w <gtg489w@gmail.com>
2016-01-02 16:09:12 -0800

    added web scaffolding

---
+++ web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name homifyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the homifyApp
+ */
+angular.module('homifyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
